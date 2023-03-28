cb50d3ff1fb7ec3e70bcf4cf3071fcd328e24b6c
enuka09 <enukapinsara09@gmail.com>
2023-03-29 00:31:37 +0530

    modif: convert signup and login html to jsp and created a servlet to retrieve registration data from the html

--- src/main/java/com/example/bumblebeeloanapplicationui/RegistrationServlet.java
+++ src/main/java/com/example/bumblebeeloanapplicationui/RegistrationServlet.java
@@ -1,26 +1,19 @@
 package com.example.bumblebeeloanapplicationui;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
-//import com.example.bumblebeeloanapplicationui.Customer;
-//import com.example.bumblebeeloanapplicationui.DBHelper;
-
-
+import com.google.gson.JsonObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 @WebServlet("/RegistrationServlet")
 public class RegistrationServlet extends HttpServlet {
-    private static final  long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -33,20 +26,46 @@ public class RegistrationServlet extends HttpServlet {
         String cusNIC = request.getParameter("CusNIC");
         LocalDate cusDOB = LocalDate.parse(request.getParameter("CusDOB"));
 
-       // Customer newCustomer = new Customer(cusFirstName, cusLastName, cusUsername, cusPassword, cusNIC, cusDOB);
+        JsonObject customerJson = new JsonObject();
+        customerJson.addProperty("firstName", cusFirstName);
+        customerJson.addProperty("lastName", cusLastName);
+        customerJson.addProperty("username", cusUsername);
+        customerJson.addProperty("password", cusPassword);
+        customerJson.addProperty("nic", cusNIC);
+        customerJson.addProperty("dob", String.valueOf(cusDOB));
 
-        try {
-          //  DBHelper dbHelper = new DBHelper();
-          //  dbHelper.addCustomer(newCustomer);
+        URL url = new URL("http://localhost:8080/BB_REST_APP-1.0-SNAPSHOT/api/customer");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoOutput(true);
 
-            out.println("<script>alert('Registration Successful!')</script>");
-            response.sendRedirect("login.jsp");
-        } catch (Exception e) {
-            out.println("<script>alert('Registration Failed!')</script>");
-            e.printStackTrace();
+        OutputStream os = connection.getOutputStream();
+        os.write(customerJson.toString().getBytes());
+        os.flush();
+
+        // Read the response from the backend resource file
+        int responseCode = connection.getResponseCode();
+        String responseBody = "";
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            // Success
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                responseBody += line;
+            }
+            br.close();
+        } else {
+            // Error
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                responseBody += line;
+            }
+            br.close();
         }
-
     }
-
-
 }
+
+
+
